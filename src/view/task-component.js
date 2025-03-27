3d028d5d91b5c0fbc4acf403e052c1625fc38224
korol933 <139908901+korol933@users.noreply.github.com>
2025-03-27 20:00:10 +0000

    Обновлены компоненты для списка задач

--- src/view/task-component.js
+++ src/view/task-component.js
@@ -1,7 +1,7 @@
 import { createElement } from '../utils/render.js';
 
 function createTaskComponentTemplate(taskText) {
-    return `<li class="task">${taskText}</li>`;
+    return `<li class="task" style="display: flex; align-items: center; justify-content: space-between; padding: 10px; border-radius: 5px; background: white; box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1); margin-bottom: 10px; width: 100%;">${taskText}</li>`;
 }
 
 export default class TaskComponent {
@@ -24,33 +24,3 @@ export default class TaskComponent {
         this.element = null;
     }
 }
-
-// src/main.js
-import HeaderComponent from './view/header-component.js';
-import FormAddTaskComponent from './view/form-add-task-component.js';
-import TaskBoardComponent from './view/task-board-component.js';
-import TaskListComponent from './view/task-list-component.js';
-import TaskComponent from './view/task-component.js';
-import { render } from './utils/render.js';
-
-const bodyContainer = document.querySelector('.board-app');
-const formContainer = document.querySelector('.add-task');
-const taskBoardContainer = document.querySelector('.taskboard');
-
-render(new HeaderComponent(), bodyContainer, 'beforebegin');
-render(new FormAddTaskComponent(), formContainer);
-const boardComponent = new TaskBoardComponent();
-render(boardComponent, taskBoardContainer);
-
-const taskLists = [];
-for (let i = 0; i < 4; i++) {
-    const taskList = new TaskListComponent();
-    render(taskList, boardComponent.getElement());
-    taskLists.push(taskList);
-}
-
-taskLists.forEach(taskList => {
-    for (let i = 0; i < 4; i++) {
-        render(new TaskComponent(`Задача ${i + 1}`), taskList.getElement());
-    }
-});
