5b83fcd73d2d302c5bc12a85c4c2a40abee57c23
korol933 <139908901+korol933@users.noreply.github.com>
2025-04-04 11:48:00 +0000

    Переделка второй лабы

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import HeaderComponent from './view/header-component.js';
 import FormAddTaskComponent from './view/form-add-task-component.js';
 import TaskBoardComponent from './view/taskboard-component.js';
 import TaskListComponent from './view/tasklist-component.js';
+import TaskComponent from './view/task-component.js';
 import {render, RenderPosition} from './framework/render.js';
 
 const container = document.querySelector('.container');
@@ -10,9 +11,21 @@ render(new HeaderComponent(), container, RenderPosition.AFTERBEGIN);
 render(new FormAddTaskComponent(), container);
 render(new TaskBoardComponent(), container);
 
-const statuses = ['backlog', 'inProgress', 'done', 'trash'];
-const taskBoard = document.querySelector('.task-board');
+const tasksData = {
+  backlog: ['Выучить JS', 'Выучить React', 'Сделать домашку'],
+  inProgress: ['Выпить сок', 'Попить воды'],
+  done: ['Позвонить маме', 'Погладить кота'],
+  trash: ['Сходить погулять', 'Прочитать Войну и Мир']
+};
 
+const statuses = ['backlog', 'inProgress', 'done', 'trash'];
 statuses.forEach(status => {
-  render(new TaskListComponent(status), taskBoard);
+  const taskList = new TaskListComponent(status);
+  const taskBoard = document.querySelector('.task-board');
+  render(taskList, taskBoard);
+
+  const tasksContainer = document.querySelector(`.${status} .task-container`);
+  tasksData[status].forEach(taskText => {
+    render(new TaskComponent(taskText), tasksContainer);
+  });
 });
