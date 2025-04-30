2790a38e3a8e0f141f810ca7920187702b3b57c8
korol933 <139908901+korol933@users.noreply.github.com>
2025-04-30 14:46:54 +0000

    добавление кнопкии очистки

--- src/view/tasklist-component.js
+++ src/view/tasklist-component.js
@@ -1,34 +1,42 @@
 import { createElement } from '../framework/render.js';
 
 function createTaskListComponentTemplate(title, tasks = []) {
+  const isTrashColumn = title === 'Корзина'; 
+  
   return `
     <div class="column ${title.toLowerCase().replace(' ', '-')}">
       <div class="column-title">${title}</div>
       <div id="${title.toLowerCase().replace(' ', '-')}-tasks">
         ${tasks.map(task => `<div class="task">${task}</div>`).join('')}
       </div>
+      ${isTrashColumn ? `
+        <button class="clear-btn">
+          ✖️ Очистить
+        </button>
+      ` : ''}
     </div>
   `;
 }
 
 export default class TaskListComponent {
   constructor(title, tasks) {
-    this.title = title;
-    this.tasks = tasks;
+    this._title = title;
+    this._tasks = tasks;
+    this._element = null;
   }
 
   getTemplate() {
-    return createTaskListComponentTemplate(this.title, this.tasks);
+    return createTaskListComponentTemplate(this._title, this._tasks);
   }
-  
+
   getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
     }
-    return this.element;
+    return this._element;
   }
 
   removeElement() {
-    this.element = null;
+    this._element = null;
   }
 }
